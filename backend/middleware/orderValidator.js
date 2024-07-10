2f9d24c60b1a1e4f857dcbf029b0e425812b47d0
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-07-10 07:16:13 -0600

    this is devun, made changes to the swagger api, followed same format as user route but implemented uniform code for the orders route. Provide validation in middleware, self configured my .env file, and lastly added to index.js file my connection to the route.

---
+++ backend/middleware/orderValidator.js
@@ -0,0 +1,36 @@
+const { body, validationResult } = require('express-validator');
+
+const orderValidationRules = () => {
+    return [
+        // fName must be a string and is required
+        body('userId').isString().notEmpty().withMessage('userId is required'),
+        // lName must be a string and is required
+        body('productIds').isArray().notEmpty().withMessage('An Array of productId is required'),
+        // email must be an email and is required
+        body('totalPrice').isFloat().notEmpty().withMessage('total is required'),
+        // phone must be a string and is required
+        body('orderDate').isString().notEmpty().withMessage('orderDate is required'),
+        // order status must be a string and is required
+        body('status').isString().notEmpty().withMessage('shippingAddress is required').matches(/^(Shipped|Processing|Cancelled)$/).withMessage('status must be either "Shipped", "Processing" or "Cancelled"'),
+        // address must be a string and is required
+        body('shippingAddress').isString().notEmpty().withMessage('address is required'),
+    ];
+};
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        return next();
+    }
+    const extractedErrors = [];
+    errors.array().map((err) => extractedErrors.push(err.msg));
+
+    return res.status(422).json({
+        errors: extractedErrors
+    });
+};
+
+module.exports = {
+    orderValidationRules,
+    validate
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 //});
 router.use("/reviews", require("./reviews"));
 router.use("/users", require("./users"));
+router.use("/orders", require("./orders"));
 router.use('/api-docs', require('./swagger'));
 
 //login/logout
