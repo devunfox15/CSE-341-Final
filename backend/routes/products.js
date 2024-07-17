70ffdc614858d08830236e606754262c25f0acaa
bcushenberry <cushenberry@gmail.com>
2024-07-18 05:02:49 +0900

    unifying product route's validation & error handling with other routes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const productController = require('../controllers/products');
+const {
+    validationRules,
+    validate
+} = require('../middleware/reviewsValidation');
 
 // List all products (GET)
 router.get('/', productController.getAll);
@@ -8,10 +12,10 @@ router.get('/', productController.getAll);
 router.get('/:id', productController.getById);
 
 // Create new product (POST)
-router.post('/', productController.createProduct);
+router.post('/', validationRules(), validate, productController.createProduct);
 
 // Update product data (PUT)
-router.put('/:id', productController.updateProduct);
+router.put('/:id', validationRules(), validate, productController.updateProduct);
 
 // Delete individual product (DELETE)
 router.delete('/:id', productController.deleteProduct);
